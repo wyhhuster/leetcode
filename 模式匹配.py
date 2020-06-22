10e68e60141f958ffc690f07d9c9b11e4558ff6a
wyhhuster <314585510@qq.com>
2020-06-22 21:11:36 +0800

    Add files via upload

---
+++ 模式匹配.py
@@ -0,0 +1,44 @@
+class Solution:
+    def patternMatching(self, pattern: str, value: str) -> bool:
+        count_a = sum(1 for ch in pattern if ch == 'a')
+        count_b = len(pattern) - count_a
+        if count_a < count_b:
+            count_a, count_b = count_b, count_a
+            pattern = ''.join('a' if ch == 'b' else 'b' for ch in pattern)
+        if not value:
+            return count_b==0
+        if not pattern:
+            return False
+
+        for len_a in range(len(value)//count_a+1):
+            rest_len=len(value)-count_a*len_a
+            if (count_b==0 and rest_len==0) or (count_b!=0 and rest_len%count_b==0):
+                len_b = 0 if count_b==0 else rest_len//count_b
+                pos=0
+                correct=True
+                value_a=''
+                value_b=''
+                for ch in pattern:
+                    if ch=='a':
+                        sub=value[pos:pos+len_a]
+                        if not value_a:
+                            value_a=sub
+                        elif value_a!=sub:
+                            correct=False
+                            break
+                        pos+=len_a
+                    if ch=='b':
+                        sub=value[pos:pos+len_b]
+                        if not value_b:
+                            value_b=sub
+                        elif value_b!=sub:
+                            correct=False
+                            break
+                        pos+=len_b
+                if correct and value_a!=value_b:
+                    return True
+        return False
+
+
+
+
