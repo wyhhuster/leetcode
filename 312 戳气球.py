ae4bcaeb54de4283aa701c27c97a50c17e5a4dba
wyhhuster <314585510@qq.com>
2020-07-19 10:07:26 +0800

    Add files via upload

---
+++ 312 戳气球.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxCoins(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        if len(nums) < 2:
+            return nums[0]
+        nums = [1] + nums + [1]
+        dp = [[0] * len(nums) for _ in range(len(nums))]
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 2, len(nums)):
+                for k in range(i + 1, j):
+                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + nums[i] * nums[k] * nums[j])
+        return dp[0][-1]
+A=Solution()
+print(A.maxCoins([3,1,5,8]))
