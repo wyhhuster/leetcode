1cf17278fa407978fbaa5286cae9dcdeb41b38ca
wyhhuster <314585510@qq.com>
2020-08-27 18:38:06 +0800

    Add files via upload

---
+++ 207 课程表.py
@@ -0,0 +1,102 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        edges = collections.defaultdict(list)
+        visited = [0] * numCourses
+        result = list()
+        valid = True
+
+        for info in prerequisites:
+            edges[info[1]].append(info[0])
+
+        def dfs(u: int):
+            nonlocal valid
+            visited[u] = 1
+            for v in edges[u]:
+                if visited[v] == 0:
+                    dfs(v)
+                    if not valid:
+                        return
+                elif visited[v] == 1:
+                    valid = False
+                    return
+            visited[u] = 2
+            result.append(u)
+
+        for i in range(numCourses):
+            if valid and not visited[i]:
+                dfs(i)
+
+        return valid
+class Solution1(object):
+
+    # 思想：该方法的每一步总是输出当前无前趋（即入度为零）的顶点
+
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int 课程门数
+        :type prerequisites: List[List[int]] 课程与课程之间的关系
+        :rtype: bool
+        """
+        # 课程的长度
+        clen = len(prerequisites)
+        if clen == 0:
+            # 没有课程，当然可以完成课程的学习
+            return True
+        # 入度数组，一开始全部为 0
+        in_degrees = [0 for _ in range(numCourses)]
+        # 邻接表
+        adj = [set() for _ in range(numCourses)]
+
+        # 想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们: [0,1]
+        # [0,1] 表示 1 在先，0 在后
+        # 注意：邻接表存放的是后继 successor 结点的集合
+        for second, first in prerequisites:
+            in_degrees[second] += 1
+            adj[first].add(second)
+
+        # print("in_degrees", in_degrees)
+        # 首先遍历一遍，把所有入度为 0 的结点加入队列
+        res = []
+        queue = []
+        for i in range(numCourses):
+            if in_degrees[i] == 0:
+                queue.append(i)
+        counter = 0
+        while queue:
+            top = queue.pop(0)
+            counter += 1
+
+            for successor in adj[top]:
+                in_degrees[successor] -= 1
+                if in_degrees[successor] == 0:
+                    queue.append(successor)
+
+        return counter == numCourses
+
+import collections
+class Solution3:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        edges = collections.defaultdict(list)
+        indeg = [0] * numCourses
+
+        for info in prerequisites:
+            edges[info[1]].append(info[0])
+            indeg[info[0]] += 1
+
+        q = collections.deque([u for u in range(numCourses) if indeg[u] == 0])
+        visited = 0
+
+        while q:
+            visited += 1
+            u = q.popleft()
+            for v in edges[u]:
+                indeg[v] -= 1
+                if indeg[v] == 0:
+                    q.append(v)
+
+        return visited == numCourses
